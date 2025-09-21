53e1fe23f5c8987f4835e43c014beb5744ecb7d4
drakoshalav-sketch <drakosha.lav@gmail.com>
2025-09-21 20:21:44 +0300

    ///

--- data_loader.py
+++ data_loader.py
@@ -1,21 +1,15 @@
 import pandas as pd
-import os
 
-# ID вашего файла с датасетом "Титаник"
 FILE_ID = "1Tj8RQHUh7MTmE88paqDBqDJZ5Ru6mOtL"
 
-# URL для прямого доступа к файлу с Google Drive
 file_url = f"https://drive.google.com/uc?export=download&id={FILE_ID}"
 
 try:
-    # Использование pandas для чтения CSV-файла напрямую по URL
     raw_data = pd.read_csv(file_url)
 
-    # Вывод информации о загрузке
     print("Датасет успешно загружен.")
     print(f"Размер датасета: {raw_data.shape[0]} строк, {raw_data.shape[1]} столбцов.")
 
-    # Выводим первые 10 строк для проверки
     print("\nПервые 10 строк датасета:")
     print(raw_data.head(10))
 
